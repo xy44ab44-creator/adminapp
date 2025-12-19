594bc12825fae74df97b298425ff3309e4485607
xy44ab44-creator <xy44ab44@gmail.com>
2025-12-19 22:17:28 +0530

    Add key

--- Test.js
+++ Test.js
@@ -3,5 +3,10 @@
     "Device Id": "fd5b7efb4a7b9975",
     "key": "KS9ARD-2U1SF1-D2K4T4-VO9HA4",
     "expiry": "2026-01-18"
+  },
+  {
+    "Device Id": "Fufuyffy",
+    "key": "CKR5C3-ZKP7IP-69G9PL-GWRN5D",
+    "expiry": "2026-01-18"
   }
 ]
