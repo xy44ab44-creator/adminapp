6bd29f63d88afa972a411843ed8140eee15998cc
xy44ab44-creator <xy44ab44@gmail.com>
2025-12-19 23:40:25 +0530

    Delete user

--- Test.js
+++ Test.js
@@ -3,10 +3,5 @@
     "Device Id": "Yyfftfcrxeseze",
     "password": "560994",
     "expiry": "2026-01-18"
-  },
-  {
-    "Device Id": "fd5b7efb4a7b9975",
-    "password": "390952",
-    "expiry": "2024-01-18"
   }
 ]
