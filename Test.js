be577a7dd937f521deba7d0e0f42a984407619a7
xy44ab44-creator <xy44ab44@gmail.com>
2025-12-20 01:26:56 +0530

    Delete user

--- Test.js
+++ Test.js
@@ -3,10 +3,5 @@
     "Device Id": "fd5b7efb4a7b9975",
     "password": "778686",
     "expiry": ""
-  },
-  {
-    "Device Id": "Hbuvuy",
-    "password": "304195",
-    "expiry": "2026-01-18"
   }
 ]
