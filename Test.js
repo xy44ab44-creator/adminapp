f3d9e91fa853072a0bf00c2088568e685f28880f
xy44ab44-creator <xy44ab44@gmail.com>
2025-12-19 23:02:13 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,17 +1,2 @@
 [
-  {
-    "password": "22",
-    "Device Id": "fd5b7efb4a46447b9975",
-    "expiry": "2026-09-29"
-  },
-  {
-    "password": "abc123",
-    "Device Id": "65d42f8e39d57060",
-    "expiry": "2025-10-15"
-  },
-  {
-    "password": "singlepass",
-    "Device Id": "65d42f8e39d57060",
-    "expiry": "" // NO DATE NO EXPIRY UNLIMITED KEY 
-  }
 ]
